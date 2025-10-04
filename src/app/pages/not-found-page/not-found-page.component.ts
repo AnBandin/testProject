c1ce9a0c41ee0c02de50eadcc687183cf7e34f4c
Andrei <bandinandrejj@yandex.ru>
2025-10-04 14:42:31 +0300

    Updated import for not-found-page

--- src/app/pages/not-found-page/not-found-page.component.ts
+++ src/app/pages/not-found-page/not-found-page.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Router } from '@angular/router';
-import { ButtonComponent } from '../../components/button/button.component';
+import {ButtonComponent} from "@ux/button/button.component";
 
 @Component({
   selector: 'app-page-not-found',
