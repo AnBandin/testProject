82d7fd388f82ce0342d140210641a9c72f818fea
Andrei <bandinandrejj@yandex.ru>
2025-10-04 14:43:26 +0300

    Added to auth Reactive Forms

--- src/app/pages/auth-page/auth-page.component.ts
+++ src/app/pages/auth-page/auth-page.component.ts
@@ -1,13 +1,46 @@
-import {Component} from '@angular/core';
-import { ButtonComponent } from '../../components/button/button.component';
+import {Component, OnInit} from '@angular/core';
+import {
+  FormBuilder,
+  FormGroup,
+  FormControl,
+  Validators,
+  ReactiveFormsModule
+} from '@angular/forms';
+import {ButtonComponent} from "@ux/button/button.component";
+import {emailOrPhoneValidator, getFieldError} from '@utils';
+import {CommonModule} from "@angular/common";
 
 @Component({
   selector: 'app-auth-page',
   standalone: true,
-  imports: [ButtonComponent],
+  imports: [ButtonComponent, ReactiveFormsModule, CommonModule],
   templateUrl: './auth-page.component.html',
   styleUrl: './auth-page.component.scss'
 })
-export class AuthPageComponent {
+export class AuthPageComponent implements OnInit {
+
+  authForm: FormGroup;
+  protected readonly getFieldError = getFieldError;
+
+  constructor(private fb: FormBuilder) {}
+
+  ngOnInit(): void {
+    this.authForm = this.fb.group({
+      emailPhone: new FormControl('', [
+        Validators.required,
+        emailOrPhoneValidator
+      ]),
+      password: new FormControl('', [
+        Validators.required,
+        Validators.minLength(5)
+      ])
+    });
+  }
+
+  onSubmit() {
+    if (this.authForm.invalid) {
+      return;
+    }
+  }
 
 }
